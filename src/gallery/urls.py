4ff4e1f9405a7c78b6e7773ebcdc8df3fdbdfbd4
Милош <milosb793@gmail.com>
2017-09-30 00:43:27 +0200

    Urls updated

--- src/gallery/urls.py
+++ src/gallery/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     # url(r'^albums/', include('src.albums.urls', namespace='albums')),
     url(r'^tags/', include('src.tags.urls', namespace='tags')),
     # url(r'^api/', include(router.urls, namespace='api')),
-    url(r'^api/albums/', include("src.albums.api.urls", namespace='albums-api'))
+    url(r'^api/albums/', include("src.albums.api.urls", namespace='albums-api')),
+    url(r'^api/images/', include("src.images.api.urls", namespace='images-api')),
+    url(r'^api/tags/', include("src.tags.api.urls", namespace='tags-api')),
 ]
 
 if settings.DEBUG:
