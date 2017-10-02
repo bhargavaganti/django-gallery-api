94d807e8c5205c9e90f621fac950f2597c07e084
Милош <milosb793@gmail.com>
2017-10-02 10:52:30 +0200

    Album API module created

--- src/albums/api/views.py
+++ src/albums/api/views.py
@@ -1,11 +1,15 @@
+from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin, CreateModelMixin
 from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 from rest_framework.filters import SearchFilter, OrderingFilter
+
+from src.gallery.helpers import log
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.permissions import AllowAny,IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from .serializers import AlbumSerializer, CreateAlbumSerializer
 from src.albums.models import Album
 from src.profiles.models import Profile
+from src.profiles.api.permissions import IsAdminOrOwner
 
 
 class GetAlbumsAPI(ListAPIView):
@@ -14,22 +18,16 @@ class GetAlbumsAPI(ListAPIView):
     добављање свих објеката, и њихово презентовање у JSON формату
     """
     serializer_class = AlbumSerializer
-    filter_backends = [SearchFilter] # ово мора бити низ!
+    filter_backends = [SearchFilter]  # ово мора бити низ!
     search_fields = ('name', 'description', 'owner__user__username', 'timestamp', 'updated')
     ordering_fields = '__all__'
 
     def get_queryset(self, *args, **kwargs):
+        pk = self.kwargs.get("pk")
+        if pk:
+            queryset_list = Album.objects.filter(owner__pk=pk)
         queryset_list = Album.objects.all()
         return queryset_list
-
-
-class GetAlbumAPI(RetrieveAPIView):
-    """
-
-    """
-    queryset = Album.objects.all()
-    serializer_class = AlbumSerializer
-    # lookup_field = 'name' # ово се може користити уместо pk, дакле уместо albums/1 може albums/ime-albuma
 
 
 class CreateAlbumAPI(CreateAPIView):
@@ -40,30 +38,21 @@ class CreateAlbumAPI(CreateAPIView):
     serializer_class = CreateAlbumSerializer
     permission_classes = [IsAuthenticated]
 
-    # FIXME: размисли да ли ово стављати
     def perform_create(self, serializer):
         profile = Profile.objects.get(user=self.request.user)
         serializer.save(owner=profile)
 
 
-class UpdateAlbumAPI(RetrieveUpdateAPIView):
+class AlbumDetailAPIView(DestroyModelMixin, UpdateModelMixin, RetrieveAPIView):
     """
 
     """
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminUser]
 
-    # FIXME: размисли да ли ово стављати
-    def perform_create(self, serializer):
-        profile = Profile.objects.get(user=self.request.user)
-        serializer.save(owner=profile)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-
-class DeleteAlbumAPI(DestroyAPIView):
-    """
-
-    """
-    queryset = Album.objects.all()
-    serializer_class = AlbumSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
