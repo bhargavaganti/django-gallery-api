a45901cb4909160bb33a61d25abcc8bc0ac56948
Милош <milosb793@gmail.com>
2017-10-03 01:52:05 +0200

    Add create routes

--- src/profiles/api/urls.py
+++ src/profiles/api/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
 from .views import GetProfilesAPI, CreateProfileAPI,ProfileDetailAPIView
-from src.albums.api.views import GetAlbumsAPI, AlbumDetailAPIView
-from src.images.api.views import GetImagesAPI, ImageDetailAPIView
-from src.tags.api.views   import GetTagsAPI, TagDetailAPIView
+from src.albums.api.views import GetAlbumsAPI, AlbumDetailAPIView, CreateAlbumAPI
+from src.images.api.views import GetImagesAPI, ImageDetailAPIView, CreateImageAPI
+from src.tags.api.views   import GetTagsAPI, TagDetailAPIView, CreateTagAPI
 from src.likes.api.views  import GetLikesAPI, LikeDetailAPIView, CreateLikeAPI
 from src.comments.api.views import GetCommentsAPI, CreateCommentAPI, CommentDetailAPIView
 
@@ -18,12 +18,15 @@ urlpatterns = [
     url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/images/(?P<image_id>\d+)/likes/?$', GetLikesAPI.as_view(),name='profile-album-image-likes'),
 
     url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/images/(?P<image_id>\d+)/tags/(?P<tag_id>\d+)/?$', TagDetailAPIView.as_view(), name='profile-album-image-tag'),
+    url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/images/(?P<image_id>\d+)/tags/create/?$', CreateTagAPI.as_view(), name='profile-album-image-tags-create'),
     url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/images/(?P<image_id>\d+)/tags/?$', GetTagsAPI.as_view(), name='profile-album-image-tags'),
 
     url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/images/(?P<image_id>\d+)/?$', ImageDetailAPIView.as_view(), name='profile-album-image'),
+    url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/images/create/?$', CreateImageAPI.as_view(), name='profile-album-images-create'),
     url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/images/?$', GetImagesAPI.as_view(), name='profile-album-images'),
 
     url(r'(?P<pk>\d+)/albums/(?P<album_id>\d+)/?$', AlbumDetailAPIView.as_view(), name='profile-album'),
+    url(r'(?P<pk>\d+)/albums/create/?$', CreateAlbumAPI.as_view(), name='albums-create'),
     url(r'(?P<pk>\d+)/albums/?$', GetAlbumsAPI.as_view(), name='albums'),
 
     url(r'(?P<pk>\d+)/?$', ProfileDetailAPIView.as_view(), name='detail'),  # TODO: обриши имена
