f6c3bfafbc3096df57aefe0cdeebcc2f2dcdde50
Милош <milosb793@gmail.com>
2017-10-07 19:49:38 +0200

    Add all separated  routes

--- src/albums/api/urls.py
+++ src/albums/api/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
+from rest_framework.compat import include
 from src.images.api.views import GetImagesAPI
 from src.images.api.views import GetAllImages
 from .views import GetAlbumsAPI, CreateAlbumAPI, AlbumDetailAPIView
 
 urlpatterns = [
-    # TODO: имплементирати овде остатак?
-    url(r'(?P<album_id>\d+)/images/?$', GetAllImages.as_view(), name='album-images'),  # TODO: обриши имена
+    # url(r'(?P<album_id>\d+)/images/(?P<image_id>\d+)/?$', GetDetailImage.as_view(), name='album-images'),
+    url(r'(?P<album_id>\d+)/images/?', include("src.images.api.urls", namespace='album-images')),
     url(r'(?P<album_id>\d+)/?$', AlbumDetailAPIView.as_view(), name='detail'),  # TODO: обриши имена
-    url(r'create/?$', CreateAlbumAPI.as_view(), name='create'),
+    # url(r'create/?$', CreateAlbumAPI.as_view(), name='create'),
     url(r'$', GetAlbumsAPI.as_view(), name='list')
 ]
 
